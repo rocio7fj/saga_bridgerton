dd4c3961e860c7e74b61659d101b42ac7d0f6d2b
rocio7fj <rocio7fj@gmailcom>
2023-01-25 15:02:17 +0100

    Cambios en funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -11,10 +11,10 @@ $(function(){
 	//FUNCIÓN para desplegar la primera sección, la de inicio, que contiene la sección introductoria a la página y la de la autora
 	function desplegarPrimero(){
 	
-		lomo.first().removeClass("collapsed");//se le quita la clase collapsed a la primera sección
-		lomo.not(":first").removeClass("desplegado");//se le quita la clase desplegado a todas menos a la primera (si alguna de ellas está desplegada se cierra)
-		lomo.first().addClass("desplegado");//se le añade la clase desplegado a la primera sección
-		lomo.not(":first").addClass("collapsed");//se le añade la clase collapsed a todas menos a la primera (se colapsan)
+		lomo.first().removeClass("collapsed");
+		lomo.not(":first").removeClass("desplegado");
+		lomo.first().addClass("desplegado");
+		lomo.not(":first").addClass("collapsed");
 		
 		if(window.matchMedia("(orientation: portrait)").matches){//si la orientación del dispositivo es portrait (diseño vertical css)
 			$('html,body').animate({scrollTop: lomo.offset().top}, 1200);//al abrirse debe hacer scroll y posicionarse arriba del todo al principio de la página
@@ -25,35 +25,36 @@ $(function(){
 
 
 	//PROGRAMA PARA HACER VISIBLE LA SECCIÓN DE INTRODUCCIÓN DENTRO DE LA SECCIÓN DE INICIO AL DESPLEGARLA HACIENDO CLICK EN EL ENLACE SAGA
-	enlaceSaga.click(function(evento){//se pone a la escucha de un click al enlace Saga Bridgerton del menú
-		evento.preventDefault();//se le indica que no realice su acción por defecto para que no navegue
-		desplegarPrimero();//se invoca la función desplegarPrimero para que se despliegue la sección de inicio
+	enlaceSaga.click(function(evento){
+		evento.preventDefault();
+		desplegarPrimero();
 		
-		seccion.first().addClass("activa");//se le añade la clase activa a la primera sección dentro de ella, la sección introductoria
-		seccion.not(":first").removeClass("activa");//se le quita la clase activa a la sección de la autora
+		seccion.first().addClass("activa");
+		seccion.not(":first").removeClass("activa");
 	});
 
+
 	//PROGRAMA PARA HACER VISIBLE LA SECCIÓN DE LA AUTORA DENTRO DE LA SECCIÓN DE INICIO AL DESPLEGARLA HACIENDO CLICK EN EL ENLACE AUTORA
-	enlaceAutora.click(function(evento){//se pone a la escucha de un click al enlace autora del menú
-		evento.preventDefault();//se le indica que no realice su acción por defecto para que no navegue
-		desplegarPrimero();//se invoca la función desplegarPrimero para que se despliegue la sección de inicio
+	enlaceAutora.click(function(evento){
+		evento.preventDefault();
+		desplegarPrimero();
 		
-		seccion.not(":first").addClass("activa");//se le añade la clase activa a la sección de la autora
-		seccion.first().removeClass("activa");//se le quita la clase activa a la sección introductoria
+		seccion.not(":first").addClass("activa");
+		seccion.first().removeClass("activa");
 	});
 
 
 	//PROGRAMA PARA DESPLEGAR LA SECCIÓN SOBRE LA QUE SE HAGA CLICK
-	lomo.click(function(ev){//se pone a la escucha de un click a todas las secciones con la clase lomo
+	lomo.click(function(ev){
 
-		if(ev.target.matches('h3 a')){//si se hace click sobre el título de uno de los libros 
-			ev.preventDefault();//para evitar que el enlace de dentro del h3 navegue por defecto
+		if(ev.target.matches('h3 a')){
+			ev.preventDefault();
 
-			if($(this).hasClass("collapsed")){//si la sección sobre la que se hace click tiene la clase collapsed (está colapsada)
-				$(this).removeClass("collapsed");//se le quita
-				$(this).siblings().removeClass("desplegado");//se quita la clase desplegado a cualquiera de las secciones hermanas que la tenga 
-				$(this).addClass("desplegado");//se le añade la clase desplegado a la sección sobre la que se hace click (se despliega)
-				$(this).siblings().addClass("collapsed");//se le añade la clase collapsed a sus hermanas (se colpasan)
+			if($(this).hasClass("collapsed")){
+				$(this).removeClass("collapsed");
+				$(this).siblings().removeClass("desplegado");
+				$(this).addClass("desplegado");
+				$(this).siblings().addClass("collapsed");
 			}
 
 			if(window.matchMedia("(orientation: portrait)").matches){//si la orientación del dispositivo es portrait (diseño vertical css)
